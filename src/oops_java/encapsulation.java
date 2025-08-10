9df5b6b8ddc9b4df50df1fc3730832c85c57993f
Tanisha parvin <tanishaparvin55@gmail.com>
2025-08-10 20:55:56 +0530

    java oops

--- src/oops_java/encapsulation.java
+++ src/oops_java/encapsulation.java
@@ -1,4 +1,26 @@
 package oops_java;
 
 public class encapsulation {
+    String name;
+    int age;
+
+    public String name() {
+        return name;
+    }
+
+    public encapsulation setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public int age() {
+        return age;
+    }
+
+    public encapsulation setAge(int age) {
+        this.age = age;
+        return this;
+    }
 }
+
+
